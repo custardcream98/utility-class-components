103725f1e373fdedeff237777997fcf6f852c64b
shiwoo.park <custardcream@kakao.com>
2023-06-18 18:59:49 +0900

    config: ignore /dist directory in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  ignorePatterns: ["node_modules/"],
+  ignorePatterns: ["node_modules/", "dist/"],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
