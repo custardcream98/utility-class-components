60948a9ce23f6a38aa9fbe4e2205c3b10cdbf3a9
shiwoo.park <custardcream@kakao.com>
2024-04-17 04:06:30 +0900

    build(web): add transform group setting

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,19 @@
 import type { Config } from "tailwindcss";
+import { transformGroupSelector } from "utility-class-components";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  plugins: [],
+  content: {
+    files: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    transform: {
+      tsx: (code) => {
+        code = transformGroupSelector(code);
+        return code;
+      },
+    },
+  },
 };
 export default config;
